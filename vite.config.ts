09c9aebe45930dcc107db734df8e03e994f4fcae
Niel Tran <66520542+nielpattin@users.noreply.github.com>
2025-07-20 01:20:18 +0700

    feat: add vite-plugin-static-copy for static file handling in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,20 @@ import devtoolsJson from 'vite-plugin-devtools-json';
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit(), devtoolsJson()]
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		devtoolsJson(),
+		viteStaticCopy({
+			targets: [
+				{
+					src: 'src/lib/server/vi_json',
+					dest: ''
+				}
+			]
+		})
+	]
 });
