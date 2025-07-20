07eeabcbe72981585f683bfda3aa0ac5aa7610b8
Niel Tran <66520542+nielpattin@users.noreply.github.com>
2025-07-20 10:40:45 +0700

    add quiz 5,6,7

--- src/lib/modules.ts
+++ src/lib/modules.ts
@@ -5,5 +5,7 @@ export const modules = [
 	{ value: '3', label: 'Module 3' },
 	{ value: '4', label: 'Module 4' },
 	{ value: '5', label: 'Module 1 - Security' },
+	{ value: '6', label: 'Module 2 - Security' },
+	{ value: '7', label: 'Module 3 - Security' },
 	{ value: 'all', label: 'All Modules' }
 ];
